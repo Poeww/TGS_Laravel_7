72fbe1a4409243ec0b82cbf511592a7758e91750
Poeww <230712423@students.uajy.ac.id>
2025-11-12 17:22:10 +0700

    feat(routes): add web crud routes bookings

--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,10 @@ Route::post('/view/packages/store', [PackageController::class, 'storeFromView'])
 Route::get('/view/packages/edit/{id}', [PackageController::class, 'edit']);
 Route::post('/view/packages/update/{id}', [PackageController::class, 'updateFromView']);
 Route::delete('/view/packages/delete/{id}', [PackageController::class, 'destroyFromView']);
+
+Route::get('/view/bookings', [BookingController::class, 'view']);
+Route::get('/view/bookings/create', [BookingController::class, 'create']);
+Route::post('/view/bookings/store', [BookingController::class, 'storeFromView']);
+Route::get('/view/bookings/edit/{id}', [BookingController::class, 'edit']);
+Route::post('/view/bookings/update/{id}', [BookingController::class, 'updateFromView']);
+Route::delete('/view/bookings/delete/{id}', [BookingController::class, 'destroyFromView']);
